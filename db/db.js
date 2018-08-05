0749fd6b031d096120c0eb52ab481d80fd0fd2a5
Benjamin Kaplan <benjaminkaplanart@gmail.com>
2018-08-05 14:48:07 -0600

    created initial server structure

---
+++ db/db.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/weatherTalk");
+mongoose.connection.on("connected", () => {
+    console.log("Mongoose Connected");
+});
+mongoose.connection.on("error", (err) => {
+    console.log(err, "Error");
+});
+mongoose.conntection.on("disconnected", () => {
+    console.log("Mongoose Disconnected")
+});
